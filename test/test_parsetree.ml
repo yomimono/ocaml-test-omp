6d8075fb0e0f70ccf0504a575a4a8728c3f9a39c
Mindy Preston <meetup@yomimono.org>
2017-11-20 21:29:54 -0600

    also test identity for structures, payloads, toplevel phrases

--- test/test_parsetree.ml
+++ test/test_parsetree.ml
@@ -6,5 +6,17 @@ let () =
              (fun expression ->
                 check_eq ~pp:(Printast.expression 0)
                   expression
-                  (Backward.copy_expression @@ Forward.copy_expression
-                     expression)))
+                  (Backward.copy_expression @@ Forward.copy_expression expression)));
+  Crowbar.(add_test ~name:"structure" [Parsetree_405.structure_to_crowbar]
+             (fun structure -> check_eq ~pp:(Printast.structure 0)
+                 structure
+                 (Backward.copy_structure @@ Forward.copy_structure structure)));
+  Crowbar.(add_test ~name:"payload" [Parsetree_405.payload_to_crowbar]
+             (fun payload -> check_eq ~pp:(Printast.payload 0)
+                 payload
+                 (Backward.copy_payload @@ Forward.copy_payload payload)));
+  Crowbar.(add_test ~name:"toplevel_phrase" [Parsetree_405.toplevel_phrase_to_crowbar]
+             (fun toplevel_phrase -> check_eq ~pp:(Printast.top_phrase)
+                 toplevel_phrase
+                 (Backward.copy_toplevel_phrase @@
+                  Forward.copy_toplevel_phrase toplevel_phrase)));
