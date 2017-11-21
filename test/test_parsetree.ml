47b1e15a56a16321849f2681213cdebd57b0c23c
Mindy Preston <meetup@yomimono.org>
2017-11-20 20:51:40 -0600

    identity test

--- test/test_parsetree.ml
+++ test/test_parsetree.ml
@@ -1,4 +1,10 @@
+module Forward = Migrate_parsetree_405_406_migrate
+module Backward = Migrate_parsetree_406_405_migrate
+
 let () =
   Crowbar.(add_test ~name:"expression" [Parsetree_405.expression_to_crowbar]
-             (fun expression -> check_eq ~pp:(Printast.expression 0)
-                 expression expression))
+             (fun expression ->
+                check_eq ~pp:(Printast.expression 0)
+                  expression
+                  (Backward.copy_expression @@ Forward.copy_expression
+                     expression)))
